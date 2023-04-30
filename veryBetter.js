42f53c79f04b46251cb6e7ee569ae37de7d47385
knottree <125846114+knottree@users.noreply.github.com>
2023-04-30 14:18:35 -0400

    cleanup

--- veryBetter.js
+++ veryBetter.js
@@ -1,6 +1,5 @@
 // databases are very... {help me out here, rest of this file...}
 import veryBetterWords from './betterWords.js';
-console.log(veryBetterWords)
 // time for some middle school JS, amigos
   let goodWord;
   let betterWord;
